b609c94e41b1eed817807efc653b5e972b00f609
Sampo Hakola <sampo.hakola@helsinki.fi>
2021-11-21 15:00:16 +0200

    osdaih

---
+++ td/tasks.py
@@ -0,0 +1,19 @@
+from invoke import task
+import os
+import platform
+
+@task
+def foo(ctx):
+    print("bar")
+
+@task 
+def start(ctx):
+    if platform.system() == 'Windows':
+        ctx.run("py src/index.py")
+    else:
+        ctx.run("python3 src/index.py")
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest")
+    
